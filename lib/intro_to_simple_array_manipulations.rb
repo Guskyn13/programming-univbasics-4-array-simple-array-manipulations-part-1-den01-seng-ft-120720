d66019d682df757c9f610a740696ec82303aeb65
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 21:01:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,12 +7,5 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  continents.pop(array)
-end
-
-def shift_with_args(array)
-
-
-def using_shift(array)
-  im_so_over_this_city = using_shift(my_favorite_cities)
+  array.pop(string)
 end
